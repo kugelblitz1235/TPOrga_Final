283d1ad0b1c838c8c7e090da46256c2b15e8722d
Tomas Tropea <tomastropea@hotmail.com>
2020-04-12 16:29:42 -0300

    Create Utility.hpp

---
+++ src/Utility.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+#include "Types.hpp"
+
+using namespace std;
+
+void printSpaceLine(int dataSize,int columns);
+void printDivisionLine(int dataSize,int columns);
+void printScoreMatrix(short* matrix,Alignment* alignment,int vec);
